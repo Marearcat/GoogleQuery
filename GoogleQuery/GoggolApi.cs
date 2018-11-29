1a4169dd97cd6c84d045b0b1d41f56e524caa17d
Rishat Latfullin <Marearcat@outlook.com>
2018-11-29 09:44:40 +0300

    GoogleCE

---
+++ GoogleQuery/GoggolApi.cs
@@ -0,0 +1,19 @@
+﻿using GoogleCSE;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace GoogleQuery
+{
+    class GoggolApi
+    {
+        public IEnumerable<GoogleSearchResult> Search(string term)
+        {
+            var gs = new GoogleSearch("016976589848602941170:e0rsagztt6w", key: "AIzaSyDx9z_1Y2x4EF6OQyiY1y2emRG8QHD2CTM");
+            //basic search
+            return gs.Search(term);
+        }
+    }
+}
---
+++ GoogleQuery/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GoogleQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new GoggolApi();
+            var sobakes = a.Search("собака");
+            Console.ReadLine();
+        }
+    }
+}
